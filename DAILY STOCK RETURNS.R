aedbb981115c3e787d15f5241afbad0bb547ff3b
Rathika Priyanka S A <rathikapriyanka0206@gmail.com>
2026-08-13 19:33:40 +0530

    Add files via upload

---
+++ DAILY STOCK RETURNS.R
@@ -0,0 +1,26 @@
+stock_data<-read.csv(file.choose())
+close<-stock_data$Close
+
+daily_return<-c(
+  NA,
+  diff(close)/head(close,-1)*100
+)
+
+daily_return
+returns<-daily_return[-1]
+
+plot(
+  daily_return[-1],
+  type = "l",
+  main = "Daily Stock Returns",
+  xlab = "Trasing Day",
+  ylab = "Daily Return(%)"
+)
+
+abline(h = 0, lty = 2 , col = "purple")
+
+best_day <-which.max(returns)
+worst_day<-which.min(returns)
+
+text(best_day,returns[best_day],"best",pos = 3 , col="darkgreen",cex = 0.7)
+text(worst_day,returns[worst_day],"worst",pos = 3,col="red",cex = 0.7)
